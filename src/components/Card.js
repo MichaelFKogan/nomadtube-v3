6cc9a41b10962f99f4fff9a5f35c098d2fd5efc1
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-08 00:19:18 -0400

    updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -98,10 +98,10 @@ function Card({ data, key, cardKey }) {
         <div className={`card ${cardSize} ${tabletSize} ${mobileSize} ${windowOpen} ${windowSize} ${cardKey}`} key={key}>
 
             <div className='auto-resizable-iframe resize'>
-                <button className="card-button close-card second-button" data-tooltip="Close" onClick={closeCard}>x</button>
-                <button className="card-button image-plus first-button" data-tooltip="Increase Image" onClick={increaseCardSize}>+</button>
-                <button className="card-button image-minus second-button" data-tooltip="Decrease Image" onClick={decreaseCardSize}>-</button>
-                <button className="card-button new-window second-button" data-tooltip="Open in window" onClick={openNewWindow}>^</button>
+                <button className="card-button close-card second-button" data-tooltip="Close" onClick={closeCard}><div class="close-card-outline"><div className="close-card-x">x</div></div></button>
+                <button className="card-button image-plus first-button" data-tooltip="Expand Image" onClick={increaseCardSize}>+</button>
+                <button className="card-button image-minus second-button" data-tooltip="Reduce Image" onClick={decreaseCardSize}>-</button>
+                <button className="card-button new-window second-button" data-tooltip="Open in window" onClick={openNewWindow}><div>→</div></button>
 
                 <div className='iframe-div'>
 
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -98,6 +98,10 @@ function Search() {
     const parseDuration = (duration) => {
         // Example: "PT1M30S" (1 minute 30 seconds)
         const match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+        if (!match) {
+            console.error(`Duration string '${duration}' is not in the expected format.`);
+            return 0; // Return a default value or handle the error as needed
+        }
         const hours = parseInt(match[1] || '0', 10) || 0;
         const minutes = parseInt(match[2] || '0', 10) || 0;
         const seconds = parseInt(match[3] || '0', 10) || 0;
