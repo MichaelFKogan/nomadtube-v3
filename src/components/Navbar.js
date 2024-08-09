ecea5a057e5bf0f23e38bccf059c0ff4f743ae6c
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-08 20:13:21 -0400

    added mobile nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,14 @@ import "../styles/navbar.css"
 
 function Navbar({toggleAsia}) {
 
+    const [openMobileMenu, setOpenMobileMenu] = useState(false);
+
+    const toggleMobileMenu  = () => {setOpenMobileMenu(!openMobileMenu);}
+    const closeMobileMenu  = () => {setOpenMobileMenu(false)};
+
     return (
-        <div className="navbar d-flex align-center space-between">
+    <>
+        <div className="navbar d-flex align-center space-between desktop">
             <div className='d-flex align-center'>
                 <Link to={"/"} className="nav-menu nav-logo desktop"><div>🏝 NomadTube</div></Link>
             </div>
@@ -26,6 +32,27 @@ function Navbar({toggleAsia}) {
             </div>
 
         </div>
+
+        <div className="navbar d-flex align-center space-between mobile">
+                <div className="nav-menu hamburger" onClick={toggleMobileMenu}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+                </div>
+
+                <Link to={"/"} className="nav-menu nav-logo "><div>🏝 NomadTube</div></Link>
+
+                <div to={"/"} className="nav-menu hamburger" style={{opacity: "0"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+                </div>
+        </div>
+        
+        <div className={`mobile-menu ${openMobileMenu ? 'd-block' : 'd-none'}`}>
+            <Link to={"/"} className="nav-menu" onClick={closeMobileMenu}><div>Home</div></Link>
+            <Link to={"/asia"} className="nav-menu" onClick={closeMobileMenu}><div>⛩ Asia</div></Link>
+            <Link to={"/southamerica"} className="nav-menu" onClick={closeMobileMenu}><div>💃🏻 South America</div></Link>
+            <Link to={"/europe"} className="nav-menu" onClick={closeMobileMenu}><div>🇪🇺 Europe</div></Link>
+            <Link to={"/middleeast"} className="nav-menu" onClick={closeMobileMenu}><div>🕋 Middle East</div></Link>
+        </div>
+    </>
     );
 };
 
