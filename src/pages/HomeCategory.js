816706b344e9dfc31ba3b58196cd34938e4bf077
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-09 15:45:35 -0400

    updates

--- src/pages/HomeCategory.js
+++ src/pages/HomeCategory.js
@@ -118,7 +118,7 @@ function HomeCategory() {
             </div> */}
 
         <div className="page-back d-flex align-center">
-            <svg xmlns="http://www.w3.org/2000/svg" style={{marginLeft: "0px"}} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" style={{marginLeft: "0px"}} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
             <Link to={"/"} className=""><div>Back</div></Link>
         </div>
 
