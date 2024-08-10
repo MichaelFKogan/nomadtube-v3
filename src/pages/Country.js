407f33afb0f977bc8d7791dbda702aeffc845ff0
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-10 11:47:03 -0400

    testing scrollable cities on mobile

--- src/pages/Country.js
+++ src/pages/Country.js
@@ -114,8 +114,11 @@ function Country() {
                 </div>
             </div>    
 
-            {/* <TotalVideos data={data}/>
-            <Breadcrumbs/> */}
+            <div className="d-flex space-between align-center mx-2 mb-5">
+                <Breadcrumbs/>
+                <TotalVideos data={data}/>
+            </div>
+
             <Cards data={data} startIndex={startIndex} endIndex={endIndex} numCardsToShow={numCardsToShow} loadMoreRef={loadMoreRef}/>
             
             {data.videos.length > 399 && (
