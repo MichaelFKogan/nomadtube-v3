e03903d1ad9f88a1832676a9ec1a0295be939e95
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-11 13:11:23 -0400

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,7 +49,7 @@ function Navbar({ toggleAsia, className }) {
             {/* MOBILE MENU */}
             <div className={`navbar d-flex align-center space-between mobile ${className}`}>
 
-                <div className='d-flex align-center' style={{ opacity: "0", visibility: "0" }}>
+                <div className='d-flex align-center'>
                     <div className="nav-menu hamburger" onClick={() => { toggleMobileMenu(); closeCountryMenus(); }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12" /><line x1="4" x2="20" y1="6" y2="6" /><line x1="4" x2="20" y1="18" y2="18" /></svg>
                     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -135,7 +135,11 @@ function Home({
             <div className='d-flex space-between black-bar-title' onClick={handleCountriesDropdown}>
                 <svg style={{opacity:"0", visibility: "0"}} xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
                 <h2 className="">🔗 Quick Links</h2>
+                ${countriesDropdown ? 
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-up"><path d="m18 15-6-6-6 6"/></svg>
+                :
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
+                }
             </div>
 
         {/* COUNTRIES */}
