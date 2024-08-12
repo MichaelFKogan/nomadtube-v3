0d441d6f1569323a791df53103b4cb1405b81fd2
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-11 20:36:41 -0400

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -53,7 +53,7 @@ function Navbar({ toggleAsia, className }) {
                     <div className="nav-menu hamburger" onClick={() => { toggleMobileMenu(); closeCountryMenus(); }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12" /><line x1="4" x2="20" y1="6" y2="6" /><line x1="4" x2="20" y1="18" y2="18" /></svg>
                     </div>
-                    {/* <Link to={"/"} className="nav-menu"><div>🏠 Home</div></Link> */}
+                    <Link to={"/"} className="nav-menu"><div>🏠</div></Link>
                 </div>
 
                 <Link to={"/"} className="nav-menu nav-logo "><div>🏝 NomadTube</div></Link>
@@ -62,7 +62,7 @@ function Navbar({ toggleAsia, className }) {
                     <div className="nav-menu hamburger" style={{ opacity: "0", visibility: "0" }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12" /><line x1="4" x2="20" y1="6" y2="6" /><line x1="4" x2="20" y1="18" y2="18" /></svg>
                     </div>
-                    {/* <Link to={"/"} className="nav-menu"><div>🏠 Home</div></Link> */}
+                    <Link to={"/"} className="nav-menu"><div>🏠</div></Link>
                 </div>
 
             </div>
