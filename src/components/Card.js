c3983a26b8b49086367980d7283bb72f78e4733c
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-11 22:30:58 -0400

    changed open in window to picture in picture

--- src/components/Card.js
+++ src/components/Card.js
@@ -108,7 +108,7 @@ function Card({ data, key, cardKey }) {
                 <button className="card-button image-minus second-button" data-tooltip="Reduce Image" onClick={decreaseCardSize}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-minus"><path d="M5 12h14"/></svg>
                 </button>
-                <button className="card-button new-window second-button" data-tooltip="Open in window" onClick={openNewWindow}>
+                <button className="card-button new-window second-button" data-tooltip="Picture In Picture" onClick={openNewWindow}>
                 {/* <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-move-up-right"><path d="M13 5H19V11"/><path d="M19 5L5 19"/></svg> */}
                 <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-square-arrow-out-up-right"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/></svg>
                 </button>
