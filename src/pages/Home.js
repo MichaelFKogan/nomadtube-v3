e298bd589429d9692afd3f6f01a8d9879245f144
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-12 20:16:09 -0400

    updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -186,11 +186,11 @@ function Home({
 
             <div className='d-flex space-between black-bar-title mobile' onClick={handleCategoriesDropdown}>
                 <h2 className="">🔗 Quick Links</h2>
-                {categoriesDropdown ? 
+                {/* {categoriesDropdown ? 
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-up"><path d="m18 15-6-6-6 6"/></svg>
                 :
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
-                }
+                } */}
             </div>
             
 
