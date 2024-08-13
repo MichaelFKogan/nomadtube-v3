aac7c78dcb299df29f22a650eb63f691d97f8c28
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-13 12:32:17 -0400

    added more data and new youtube api keys

--- src/components/QuickLinkCountries.js
+++ src/components/QuickLinkCountries.js
@@ -57,7 +57,7 @@ function QuickLinkCountries({ className }) {
 
             <div className={`quicklinks-countries ${asiaMenu ? 'd-flex' : 'd-none' }`}>
                 <button id="close-quick-links" className="card-button close-card second-button" data-tooltip="Close" onClick={closeAllMenus}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-x mobile"><circle cx="12" cy="12" r="10"/><path d="m15 9-6 6"/><path d="m9 9 6 6"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-x mobile"><circle cx="12" cy="12" r="10"/><path d="m15 9-6 6"/><path d="m9 9 6 6"/></svg>
                 </button>
                 <div className="inner">
                     <AsiaMobileMenu asiaMenu={asiaMenu} toggleAsiaMenu={toggleAsiaMenu} back={back} closeCountryMenus={closeCountryMenus} closeAllMenus={closeAllMenus}/>
