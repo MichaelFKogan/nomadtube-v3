37839b37ed23e7e1e8fbcf8c408b4853b5b0baf0
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-13 18:55:06 -0400

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -87,7 +87,7 @@ function Navbar({ toggleAsia, className, scrollToTop, countryMenu }) {
 
             <div className={`mobile-menu ${mobileMenu ? 'd-block' : 'd-none'}`}>
                 <Link to={"/"} className="nav-menu" onClick={closeAllMenus}><div>🏠 Home</div></Link>
-                <div className="nav-menu" onClick={() => { closeMobileMenu(); toggleAsiaMenu(); }} style={{ marginLeft: "0px", paddingTop:"5px", paddingBottom: "27px" }}><div>🔗 Quick Links</div></div>
+                {/* <div className="nav-menu" onClick={() => { closeMobileMenu(); toggleAsiaMenu(); }} style={{ marginLeft: "0px", paddingTop:"5px", paddingBottom: "27px" }}><div>🔗 Quick Links</div></div> */}
                 <hr className="w-95" style={{marginRight: "auto", marginLeft: "0px", marginBottom: "25px"}}/>
 
                 <h2 className="" style={{color: "white", fontSize: "20px", textAlign: "left", marginBottom: "20px"}}>🌎 Countries</h2>
