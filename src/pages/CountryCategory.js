0f9beb2fefec2c5d0cba1d0c3d6f6315aec935d7
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-14 21:55:56 -0400

    updates

--- src/pages/CountryCategory.js
+++ src/pages/CountryCategory.js
@@ -124,7 +124,7 @@ function CountryCategory() {
             </div>
 
         {/* CATEGORY TITLE */}
-            {dataCategories.categories.map((item, index) => (category === item.route ? <div className="category-title"><div className="subtitle">{dataCities.name}</div><h2>{item.name}</h2></div> : null))}  
+            {dataCategories.categories.map((item, index) => (category === item.route ? <div className="category-title"><div className="subtitle">{dataCities.noemoji}</div><h2>{item.name}</h2></div> : null))}  
 
         {/* <div className="page-back d-flex align-center mobile">
             <svg xmlns="http://www.w3.org/2000/svg" style={{marginLeft: "0px"}} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
