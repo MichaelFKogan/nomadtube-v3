bee2655c6bb24d9f256584868f51de6f5c7fe2bb
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-18 14:34:37 -0400

    updates

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,12 +3,12 @@ import Card from "../components/Card"
 
 function Cards({data, startIndex, endIndex}) {
 
-    const [layout, setLayout] = useState("masonry");
+    const [layout, setLayout] = useState("full-width");
 
     const toggleLayout = (prevClass) => {
         setLayout((prevClass) => {
             if (prevClass === 'full-width') return 'grid';
-            if (prevClass === 'grid') return 'masonry';
+            // if (prevClass === 'grid') return 'masonry';
             return 'full-width';
         });
     }
@@ -28,7 +28,8 @@ function Cards({data, startIndex, endIndex}) {
                 )}
 
                 {layout === 'grid' && (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-table-cells-merge"><path d="M12 21v-6"/><path d="M12 9V3"/><path d="M3 15h18"/><path d="M3 9h18"/><rect width="18" height="18" x="3" y="3" rx="2"/></svg>
+                    // <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-table-cells-merge"><path d="M12 21v-6"/><path d="M12 9V3"/><path d="M3 15h18"/><path d="M3 9h18"/><rect width="18" height="18" x="3" y="3" rx="2"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-rows-2"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M3 12h18"/></svg>
                 )} 
 
                 {layout === 'masonry' && (
