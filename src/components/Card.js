9de3c4e1386abf43a9b98c1bf77636538f990b57
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-18 14:13:48 -0400

    updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -122,7 +122,7 @@ function Card({ data, key, cardKey }) {
                 </button>
 
                 <button className="card-button rotate-card" data-tooltip="Rotate Card" onClick={rotateCard}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-redo-2"><path d="m15 14 5-5-5-5"/><path d="M20 9H9.5A5.5 5.5 0 0 0 4 14.5A5.5 5.5 0 0 0 9.5 20H13"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-redo"><path d="M21 7v6h-6"/><path d="M3 17a9 9 0 0 1 9-9 9 9 0 0 1 6 2.3l3 2.7"/></svg>
                 </button>
 
                 <div className='iframe-div'>
