cdccf909a65a6a43342b5990adfdb10f2f7fa703
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-08-19 23:04:02 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -99,7 +99,8 @@ function App() {
 
               {/* BOTTOM NAVIGATION BUTTONS */}
               <button className="back-to-top mobile" onClick={() => {window.scrollTo({top: 0, behavior: 'smooth'})}}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-arrow-up"><circle cx="12" cy="12" r="10"/><path d="m16 12-4-4-4 4"/><path d="M12 16V8"/></svg>
+                Back To Top
+                {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-arrow-up"><circle cx="12" cy="12" r="10"/><path d="m16 12-4-4-4 4"/><path d="M12 16V8"/></svg> */}
               </button>
     </div>
   );
